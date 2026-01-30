83d0a9dfe739260e4289473453398b270ba1eeab
varshagk2709 <160628276+varshagk2709@users.noreply.github.com>
2026-01-30 10:45:27 +0530

    Update MainClass.java

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -1,16 +1,28 @@
-package com.wip;
+ppackage com.wip;
 
 public class MainClass {
-
+	
 	public int add(int a, int b) {
-		return a + b;
+	
+		return a+b;
 	}
 
 	public static void main(String[] args) {
 		MainClass mc = new MainClass();
-		System.out.println(mc.add(10, 20));
-		System.out.println("Java For Docker!!!! . THis code is pulled from WIP GitHub Repository");
-		System.out.println("This is the updated code after adding Jenkinsfile to the repository");
+		while(true) {
+			System.out.println(mc.add(10, 20));
+			System.out.println("Java for Docker! Code is pulled from GitHub Repository");
+			System.out.println("Updated code after adding Jenkins file to repository");
+			try {
+				Thread.sleep(2000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
 	}
+
+	
 
 }
